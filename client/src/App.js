2ec6c83eb4cedd1452df73682a0a47f4a72cbff1
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-01 17:17:40 +0200

    Build: install react starter package

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <header className="header">Header</header>
+      <main className="main">Main</main>
+      <footer className="footer">Footer</footer>
+    </>
+  );
+}
