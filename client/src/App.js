7133d8179de59a0f6c2ce2d0ddea6bd7e63df574
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-10 14:36:43 +0200

    Feature: create a loader component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { CircularProgress } from "@mui/material";
+import Loader from "./components/Loader/Loader";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
@@ -9,19 +9,7 @@ const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
 export default function App() {
   return (
     <BrowserRouter>
-      <Suspense
-        fallback={
-          <CircularProgress
-            style={{
-              margin: 0,
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              color: "#b3befe"
-            }}
-          />
-        }
-      >
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<ErrorPage />} />
