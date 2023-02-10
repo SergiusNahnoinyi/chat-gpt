9597a63040d1394a9a02fd8d5aa023a75281ddea
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-10 14:28:19 +0200

    Feature: implement routing and add pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import "./App.css";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { CircularProgress } from "@mui/material";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
 
 export default function App() {
   return (
-    <>
-      <header className="header">
-        <h1>ChatGPT</h1>
-      </header>
-      <main className="main">
-        <aside className="sidemenu">Aside</aside>
-        <section className="chat">Section</section>
-      </main>
-      <footer className="footer">
-        <p>Not affiliated with or endorsed by OpenAI</p>
-      </footer>
-    </>
+    <BrowserRouter>
+      <Suspense
+        fallback={
+          <CircularProgress
+            style={{
+              margin: 0,
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              color: "#b3befe"
+            }}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
---
+++ client/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,5 @@
+import "./ErrorPage.css";
+
+export default function ErrorPage() {
+  return <div className="error">ErrorPage</div>;
+}
