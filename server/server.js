3b837c3f205ad7bd0a68bcc0ad4f257cbefc31d6
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-14 10:58:04 +0200

    Refactoring: change quantity of the requests

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post("/", async (req, res) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${message}`,
-    max_tokens: 50,
+    max_tokens: 100,
     temperature: 0.5
   });
 
