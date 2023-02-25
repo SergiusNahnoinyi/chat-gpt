7ddf0b2268a62328e1eadf6524ae4e03215317ca
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-25 18:45:32 +0200

    Refactoring: move loader styles to the separate file

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,15 +1,7 @@
 import { CircularProgress } from "@mui/material";
 
+import "./Loader.css";
+
 export default function Loader() {
-  return (
-    <CircularProgress
-      style={{
-        margin: 0,
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        color: "#b3befe"
-      }}
-    />
-  );
+  return <CircularProgress className="loader" />;
 }
